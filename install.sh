56ffb1e4d8017e91d23e9756ed28c328acde38f4
tyoras <tyoras@gmail.com>
2017-03-11 23:58:04 +0100

    git config

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# vim
+#ln -s ${BASEDIR}/vimrc ~/.vimrc
+#ln -s ${BASEDIR}/vim/ ~/.vim
+
+# zsh
+#ln -s ${BASEDIR}/zshrc ~/.zshrc
+
+# git
+ln -sfv ${BASEDIR}/git/.gitconfig ~
+ln -sfv ${BASEDIR}/git/.gitignore_global ~
+
