ba78d57f201ff620d3d6bdb47b98492b950dfb15
tyoras <tyoras@gmail.com>
2023-12-06 14:20:39 +0100

    Neovim config from scratch

--- install.sh
+++ install.sh
@@ -19,23 +19,6 @@ sudo dpkg -i /tmp/delta.deb
 # fzf
 sudo apt install -y fzf ripgrep universal-ctags silversearcher-ag fd-find
 
-# vim
-echo -e "\n- Installing vim settings"
-sudo apt install -y vim
-#ln -s ${BASEDIR}/vimrc ~/.vimrc
-#ln -s ${BASEDIR}/vim/ ~/.vim
-# install pathogen
-mkdir -p ~/.vim/autoload ~/.vim/bundle && \
-curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
-# install vim-airline
-git clone https://github.com/vim-airline/vim-airline ~/.vim/bundle/vim-airline
-# install nerdtree
-git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
-# install nerdtree-git-plugin 
-git clone https://github.com/Xuyuanp/nerdtree-git-plugin.git ~/.vim/bundle/nerdtree-git-plugin
-# install vim-devicons
-git clone https://github.com/ryanoasis/vim-devicons ~/.vim/bundle/vim-devicons
-
 # zsh
 echo -e "\n- Installing zsh settings"
 sudo apt install -y zsh
@@ -43,26 +26,9 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/mas
 ./tools/zsh/tyoras-agitnoster-theme.sh
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
-# node + yarn
-./tools/nodejs.sh
-npm i -g yarn
-
-# ranger
-./tools/ranger.sh
-
 # neovim
 echo -e "\n- Installing neovim settings"
 sudo snap install nvim --classiic
-#pip install pynvim
-#npm i -g neovim
-#mkdir -p ~/.config/coc/extensions
-#cd ~/.config/coc/extensions
-#if [ ! -f package.json ]
-#then
-#  echo '{"dependencies":{}}'> package.json
-#fi
-## Change extension names to the extensions you need
-#npm install coc-snippets coc-python coc-tsserver coc-html coc-css coc-json coc-yaml coc-metals coc-explorer --global-style --ignore-scripts --no-bin-links --no-package-lock --only=prod
 cd ${BASEDIR}
 
 #tmux
@@ -75,6 +41,8 @@ sudo apt install -y htop
 ./tools/ctop.sh
 ./tools/postman.sh
 ./tools/jvm.sh
+./tools/nodejs.sh
+npm i -g yarn
 
 # stow dotfiles
 echo -e "\n- Starting to stow dotfiles"
